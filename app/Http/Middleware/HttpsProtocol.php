75cf0e88824734131612c50c8101e7243d4808a4
GHARIBMOHAMED <gharibmalali@gmail.com>
2022-11-04 11:43:53 +0100

    https redirect

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http' && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }else {
+            return $next($request);
+        }
+    }
+
+    
+}
